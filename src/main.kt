b99ce47ae2a6deae0349b404d0b0044cf3a92ad9
Liliane-Gikuku <lilkundiro@gmail.com>
2022-03-12 08:25:40 +0300

    changes

--- src/main.kt
+++ src/main.kt
@@ -1,41 +1,47 @@
 fun main(){
 
-     cousins (arrayOf("Lilian","Tita","Teta","Zion"))
+     cousins("Lilian","Tita","Teta","Zion")
     cities()
-    value()
-    state(arrayOf("yves","chance","anne"))
 
-}
-//Question 1
-fun cousins(name:Array<String>){
-println(name.contentToString())
+     value(arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62))
 
-}
-
-//Question 2
-fun cities(){
-    var places=arrayListOf("harare","mumbai","dodoma","jakarta")
-    places.forEach { city->
-        println(city.capitalize())
-
-    }
-}
-//Question 3
-fun value(){
-    var values=arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
-    var sum=values[1]+values[4]
-    println(sum)
-    var index=values.indexOf(158)
-    println(index)
-    var element=values.sortedArray()
-    println(element.contentToString())
-
+    var name = state("Lilian", "Ganza","Teta")
+    println(name.contentToString())
 
 }
 
 //Question 4
-fun state(state:Array<String>):Array<String>{
-    var yuppies=state
-    println(yuppies.contentToString())
-    return yuppies
+fun state(a:String,b:String,c:String):Array<String>{
+    var name=arrayOf(a,b,c)
+    return name
 }
+//Question 1
+fun cousins(a:String,b:String,c:String,d:String){
+    var names= arrayOf(a,b,c,d)
+     println(names.contentToString())
+
+}
+
+//Question 2
+fun cities() {
+    var places = arrayListOf("harare", "mumbai", "dodoma", "jakarta")
+    places.forEach { city ->
+        println(city.capitalize())
+
+    }
+}
+
+    fun  value(numbers:Array<Int>){
+    var values=arrayOf(numbers)
+    println(values)
+
+    var sum=numbers[1]+ numbers[4]
+    println(sum)
+    var index=numbers.indexOf(158)
+    println(index)
+    println(numbers.sortedArray().contentToString())
+
+
+}
+
+
